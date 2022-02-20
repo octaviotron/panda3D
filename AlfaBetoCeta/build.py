a5eb5148b7a7ad986ce251e192ce55f878ef8ccc
Octavio Rossell <octavio.rossell@gmail.com>
2022-02-19 20:32:56 -0400

    Recipes for building binaries

---
+++ AlfaBetoCeta/build.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='AlfaBetoCeta',
+    options={
+        'build_apps': {
+            'gui_apps': {
+                'AlfaBetoCeta': 'index.py',
+            },
+			'build_base': "../build_alfaBetoCeta",
+            'log_filename': 'output.log',
+            'log_append': False,
+            'include_patterns': [
+                '**/*.bam',
+				'**/*.wav',
+				'**/*.ttf'
+            ],
+            'plugins': [
+                'pandagl',
+                'p3openal_audio',
+            ],
+			'platforms': ['manylinux1_x86_64', 'win_amd64'],
+			'log_filename' : "log.log"
+        }
+    }
+)
