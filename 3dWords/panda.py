cd711e6e4e7d0de0f780299ab882392c99150cf8
Octavio Rossell <octavio.rossell@gmail.com>
2022-04-28 22:05:10 -0400

    Example code

---
+++ 3dWords/panda.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#   This program is free software: you can redistribute it and/or modify it 
+#   under the terms of the GNU General Public License as published by the 
+#   Free Software Foundation, either version 3 of the License, or 
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of 
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along 
+#   with this program. If not, see <https://www.gnu.org/licenses/>. 
+#
+#   Developed by Octavio Rossell Tabet <octavio.rossell@gmail.com>
+#
+
+import sys
+import gltf
+
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import AmbientLight, Vec4
+
+from libs import word3d
+
+class ElEnvBase(ShowBase):
+	def __init__(self):
+		ShowBase.__init__(self)
+		gltf.patch_loader(self.loader)
+		self.Main()
+
+		self.accept('escape', self._Out)
+
+	def Main(self):
+		camara = render.attachNewNode("CAMERA")
+		camara.setY(-20)
+		base.camera.reparentTo(camara)
+
+		alight = AmbientLight('Ambient')
+		alight.setColor(Vec4(0.5,0.5,0.5, 1))
+		alnp = render.attachNewNode(alight)
+		render.setLight(alnp)
+		
+		w3d = word3d.Word3d()
+		w3d.fontname = "DejaVuSansMono"
+		w3d.center = True
+		w3d.color = (0,0,1,1)
+		w3d.scale = 1
+		w3d.spacing = 0
+		label = w3d.Word("Test")
+		label.reparentTo(render)
+
+
+	def _Out(self):
+		sys.exit()
+
+
+
+
+app = ElEnvBase()
+app.run()
+
